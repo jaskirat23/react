3129bf66218a15b0efc36bf7f35adc1ad0b7f947
jaskirat23 <singh.jaskirat23@gmail.com>
2020-07-27 16:15:48 +0530

    Updates

---
+++ precache-manifest.ed3b933bb8a2325458eceeeea560fce8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e04e3f274e68f8e54eb16204395f43d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ddee30ac1fdee87b16d6",
+    "url": "/static/css/main.0b398dee.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddee30ac1fdee87b16d6",
+    "url": "/static/js/main.be462131.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
